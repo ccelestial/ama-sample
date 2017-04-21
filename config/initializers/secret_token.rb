681c826a8336c1185e400101dd6a89416ec2c974
ccelestial <celestialchristopher@gmail.com>
2017-04-21 13:20:51 +0800

    Initializing App. For AWS SDK and Mobile Analytics Study

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmaSample::Application.config.secret_token = 'c001595d2d2219f83dfd2c624e0fb5f9e9d7de999e4d2644876860359382d2ad1872dbfb4a07e5f580f47b1bf183013ac1b037747ca2f59ba0e8ed0c394024fa'
