766390396eea5d1e7623df12ec870499082f2542
ccelestial <celestialchristopher@gmail.com>
2017-04-26 07:46:43 +0800

    Updated AwsmaWorker to adapt in batch event sending

--- app/workers/awsma_worker.rb
+++ app/workers/awsma_worker.rb
@@ -1,22 +1,14 @@
 class AwsmaWorker
   include Sidekiq::Worker
 
-  def perform(client_id, session_id, event_name)
+  def perform(client_id, events)
+    puts ">>>>>> Now Sending: #{client_id} => #{events.first["session_id"]}, Event Count: #{events.count}"
     app_title = "Team App"
     app_package_name = "com.ta.team_app"
 
-    attributes = {}
-    metrics = {}
-
     client = AwsmaClient.get_client
 
-    result = client.report_event(client_id,
-                        session_id,
-                        app_title,
-                        app_package_name,
-                        event_name,
-                        attributes,
-                        metrics)
+    result = client.report_events(client_id, app_title, app_package_name, events)
 
     puts result
 
