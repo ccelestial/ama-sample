78ad1e06bc62641589758e80df1b812e481b134c
ccelestial <celestialchristopher@gmail.com>
2017-04-26 07:46:15 +0800

    Added functionality for sending batch events

--- app/models/awsma_client.rb
+++ app/models/awsma_client.rb
@@ -1,12 +1,16 @@
 class AwsmaClient
+  protected
+
+  @@events = {}
+
+  public
+
   def self.get_client
     @client ||= self.new.get_client
   end
 
   def initialize
     awsma = Rails.application.awsma
-    puts ">>>>> This is the Client"
-    puts @client
 
     @client = AwsmaRails::Reporter.new(
         awsma[:url],
@@ -16,5 +20,36 @@ class AwsmaClient
 
   def get_client
     @client
+  end
+
+  def self.add_event(client_id, session_id, event_name, attributes, metrics)
+    @@events["#{client_id}:#{session_id}"] ||= []
+
+    new_event = {
+        'client_id' => client_id,
+        'session_id' => session_id,
+        'event_name' => event_name,
+        'attributes' => attributes,
+        'metrics' => metrics
+    }
+
+    @@events["#{client_id}:#{session_id}"] << new_event
+
+    if @@events["#{client_id}:#{session_id}"].count >= 10
+      self.send_to_analytics(client_id, @@events["#{client_id}:#{session_id}"])
+      @@events.except!("#{client_id}:#{session_id}")
+    end
+
+    new_event
+  end
+
+  def self.events
+    @@events
+  end
+
+  private
+
+  def self.send_to_analytics(client_id, events)
+    AwsmaWorker.perform_async(client_id, events)
   end
 end
